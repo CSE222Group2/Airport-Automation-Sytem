6ab0359f5aa6e6c62329cbe1d838defeecba9d5a
Omer Faruk <omerfsayar@gmail.com>
2020-06-24 14:29:38 +0300

    Update Airport.java

--- src/cse222/proje/Airport.java
+++ src/cse222/proje/Airport.java
@@ -1,8 +1,6 @@
 package cse222.proje;
 
-import java.util.LinkedList;
-import java.util.NoSuchElementException;
-import java.util.Queue;
+import java.util.*;
 
 public class Airport {
 
@@ -15,24 +13,27 @@ public class Airport {
 	 Queue<Flight> flightOrder;
 
 	public Airport() {
+		airportName = "Sabiha Gokcen";
 		administrator = new Administrator("murat","yildiz",1801042004, "cse222");
 		sectors = new LinkedList<>();
 		cleanningPersonels = new AVLTree<>();
 		securityPersonels = new AVLTree<>();
 		firms = new LinkedList<>();
+		flightOrder = new LinkedList<>();
 	}
 
 	/**
 	 * Creates an Airport object and creates first Administrator of Airport
 	 * @param airpotName will be set
 	 */
-	public Airport(String airpotName){
+	public Airport(String airpotName, Administrator admin){
 		this.airportName = airpotName;
-		administrator = new Administrator("murat","yildiz",1801042004, "cse222");
+		administrator = admin;
 		sectors = new LinkedList<>();
 		cleanningPersonels = new AVLTree<>();
 		securityPersonels = new AVLTree<>();
 		firms = new LinkedList<>();
+		flightOrder = new LinkedList<>();
 
 	}
 
@@ -162,7 +163,7 @@ public class Airport {
 		public Sector removeSector(Sector sec){
 			return sectors.remove(sectors.indexOf(sec));
 		}
-		
+
 		/**
 		 *  Returns true if successfully remove given Firm, otherwise false
 		 * @param firm will be removed
@@ -237,6 +238,7 @@ public class Airport {
 		}
 	}
 
+
 	/**
 	 * Returns Administrator which has given ID and password, if not exist returns null
 	 * @param ID will be checked
@@ -244,7 +246,14 @@ public class Airport {
 	 * @return Administrator which has given ID and password, if not exist returns null
 	 */
 	public Administrator findAdministrator(int ID, String password){
-		return new Administrator("a","b", 1, "a");
+		if(administrator.ID == ID && administrator.password.equals(password))
+			return administrator;
+		else {
+			System.out.println("No admin has been found! ");
+			return null;
+		}
+
+
 	}
 
 	/**
@@ -254,7 +263,16 @@ public class Airport {
 	 * @return SecurityPersonel which has given ID and password, if not exist returns null
 	 */
 	public SecurityPersonel findSecurityPersonel(int ID, String password){
-		return new SecurityPersonel("a","b", 1, "a");
+		Iterator itr = securityPersonels.iterator();
+		SecurityPersonel e = null;
+		while(itr.hasNext()){
+			e = (SecurityPersonel) itr.next();
+			if(e.ID == ID && e.password.equals(password))
+				return e;
+
+
+		}
+		return null;
 	}
 
 	/**
@@ -264,28 +282,50 @@ public class Airport {
 	 * @return CleanningPersonel which has given ID and password, if not exist returns null
 	 */
 	public CleanningPersonel findCleanningPersonel(int ID, String password){
-		return new CleanningPersonel("a","b", 1, "a");
-	}
+		Iterator itr = cleanningPersonels.iterator();
+		CleanningPersonel e = null;
+		while(itr.hasNext()){
+			e = (CleanningPersonel) itr.next();
+			if(e.ID == ID && e.password.equals(password))
+				return e;
 
+
+		}
+		return null;
+	}
 	/**
 	 * Returns Sector which has given ID and password, if not exist returns null
 	 * @param ID will be checked
-	 * @param password will be checked
+
 	 * @return Sector which has given ID and password, if not exist returns null
 	 */
-	public Sector findSector(int ID, String password){
-		return new Sector();
+	public Sector findSector(int ID){
+		ListIterator<Sector> itr = sectors.listIterator();
+		Sector temp;
+		while(itr.hasNext()){
+			temp = itr.next();
+			if(temp.getSectorID() == ID)
+				return temp;
+		}
+		return null;
 	}
 
 	/**
 	 * Returns Firm which has given ID and password, if not exist returns null
-	 * @param ID will be checked
-	 * @param password will be checked
+	 * @param firmNAme will be checked
+
 	 * @return Firm which has given ID and password, if not exist returns null
 	 */
-	public Firm findFirm(int ID, String password){
-		return new Firm("a");
+	public Firm findFirm( String firmNAme){
+		ListIterator<Firm> itr = firms.listIterator();
+		while(itr.hasNext()){
+			Firm temp = itr.next();
+			if(temp.getFirmName().equals(firmNAme))
+				return temp;
+		}
+		return null;
 	}
+
 
 	/**
 	 * Returns flight data which flight has given flight ID
@@ -293,9 +333,19 @@ public class Airport {
 	 * @return flight data which flight has given flight ID, otherwise null
 	 */
 	public String getFlightData(int flighID){
-		return "";
+		ListIterator<Firm> itr = firms.listIterator();
+		while(itr.hasNext()){
+			Firm temp = itr.next();
+			if(temp.findFlight(flighID) != null){
+				return temp.toString();
+			}
+		}
+		return null;
 	}
-
+	
+	/**
+	*Getter for Administrator
+	*/
 	public Administrator getAdministrator() {
 		return administrator;
 	}
